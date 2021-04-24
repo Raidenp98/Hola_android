720063ce80c9de180072793bbe8718888127c05c
Juan Palma <megazerox98@gmail.com>
2021-04-23 23:01:27 -0400

    Boton Nuclear nuevo

--- src/app/reactor-n/reactor-n.module.ts
+++ src/app/reactor-n/reactor-n.module.ts
@@ -4,15 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
 import { ReactorNPageRoutingModule } from './reactor-n-routing.module';
 
 import { ReactorNPage } from './reactor-n.page';
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    ExploreContainerComponentModule,
     ReactorNPageRoutingModule
   ],
   declarations: [ReactorNPage]
